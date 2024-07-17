d74b39f556b7a41036b9888ab5d07377cd7ae09a
SlayerK2024 <157855730+SlayerK2024@users.noreply.github.com>
2024-07-17 17:08:54 -0400

    Update index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,7 @@
 const htmlController = require('./htmlController');
 const userController = require('./userController');
 const blogController = require('./blogController');
+const commentController = require('./commentController')
+const dashboardController = require('./dashboardController')
 
-module.exports = { htmlController, userController, blogController };
+module.exports = { htmlController, userController, blogController, commentController, dashboardController };
