534b83e195fc863d26e5dff605089b48e8102baa
SlayerK2024 <157855730+SlayerK2024@users.noreply.github.com>
2024-07-17 17:17:54 -0400

    Update blogRoutes.js

--- routes/apiRoutes/blogRoutes.js
+++ routes/apiRoutes/blogRoutes.js
@@ -1,8 +1,24 @@
 const blogController = require('../../controllers/blogController');
+const commentController = require('../controllers/commentController');
 const { withAuth } = require('../../utils/Auth');
 
 const router = require('express').Router();
 
 router.route('/').post(withAuth, blogController.add);
+
+router.get('/', blogController.getAllPosts);
+
+
+router.get('/BlogPost/:id', blogController.getPostById);
+
+
+router.post('/BlogPost', blogController.createPost);
+
+
+router.post('/BlogPost/:id/comment', commentController.createComment);
+
+
+router.delete('/BlogPost/:id', blogController.deletePost);
+
 
 module.exports = router;
